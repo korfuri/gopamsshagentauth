eba769acfaad722d8142ed8b4e192ea07872578a
Uriel Corfa <korfuri@gmail.com>
2022-09-17 18:36:54 +0200

    Simplify the PAM module loader

--- pam/pam.go
+++ pam/pam.go
@@ -7,29 +7,12 @@ import "C"
 
 import (
 	"flag"
-	"fmt"
-	"log"
-	"unsafe"
 
 	gpsa "github.com/korfuri/gopamsshagentauth"
 )
 
-type PamHandle struct {
-	p unsafe.Pointer
-}
-
-func (p PamHandle) ptr() *C.pam_handle_t {
-	return (*C.pam_handle_t)(p.p)
-}
-
-func getHandle(h *C.pam_handle_t) PamHandle {
-	return PamHandle{unsafe.Pointer(h)}
-}
-
 //export authenticate
-func authenticate(handle *C.pam_handle_t, pamflags C.int, argv []string) C.int {
-	fmt.Printf("authenticate: %v", argv)
-
+func authenticate(pamhandle *C.pam_handle_t, pamflags C.int, argv []string) C.int {
 	var cfg gpsa.AgentAuthConfig
 	flags := flag.NewFlagSet("gopamsshagentauth", flag.PanicOnError)
 	flags.StringVar(&cfg.AuthorizedKeysFile, "authorized_keys_file", "", "path to an authorized_keys file")
@@ -37,33 +20,36 @@ func authenticate(handle *C.pam_handle_t, pamflags C.int, argv []string) C.int {
 	flags.StringVar(&cfg.AuthorizedPrincipals, "authorized_principals", "", "comma-separated list of authorized principals")
 	flags.StringVar(&cfg.AuthorizedPrincipalsFile, "authorized_principals_file", "", "path to a file containing a list of authorized principals")
 
+	// PAM convention is to omit the dashes on flags. Add them back.
 	for i := range argv {
 		argv[i] = "--" + argv[i]
 	}
 	if err := flags.Parse(argv); err != nil {
-		log.Fatalf("unable to parse flags")
+		return C.PAM_SERVICE_ERR
 	}
 
-	a, err := gpsa.NewAgentAuth(cfg)
+	a, err := gpsa.NewAgentAuth(cfg) // TODO we must drop euid here!
 	if err != nil {
-		log.Fatalf("NewAgentAuth: %s", err)
+		return C.PAM_SERVICE_ERR // TODO should we make a difference
+		// between errors loading keys and errors opening the agent socket?
 	}
-
 	defer a.Close()
 
 	candidates, err := a.FilterCandidates()
 	if err != nil {
-		log.Fatalf("filterCandidates: %v", err)
+		return C.PAM_AUTH_ERR
 	}
 
 	result, err := a.ChallengeKeys(candidates)
 	if err != nil {
-		log.Fatalf("challengeKeys: %v", err)
+		return C.PAM_AUTH_ERR
 	}
 	if !result {
-		log.Fatalf("Result: go away")
-	} else {
-		log.Printf("Result: welcome")
+		return C.PAM_AUTH_ERR
+	}
+
+	if err != nil {
+		return C.PAM_CONV_ERR
 	}
 	return C.PAM_SUCCESS
 }
